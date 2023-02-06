5f5726552c5a3dd6fdedeeb89efa4ad0733dc825
José Augusto <joseaugustogomessantos12@gmail.com>
2023-02-06 16:56:49 -0300

    alterando um erro

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function media(){
     var nota1 = parseFloat(document.getElementById("nota1").value);
     var nota2 = parseFloat(document.getElementById("nota2").value);
-    v
+    
 
     var media = (nota1 + nota2)/2 ;
 
