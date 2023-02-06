83397234372939f58d8c4fae490a3bf59d713ebb
José Augusto <joseaugustogomessantos12@gmail.com>
2023-02-06 16:50:32 -0300

    Aplicando o CSS

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function media(){
     var nota1 = parseFloat(document.getElementById("nota1").value);
     var nota2 = parseFloat(document.getElementById("nota2").value);
+    v
 
     var media = (nota1 + nota2)/2 ;
 
+
     if(media >= 6)
      if(media==10)
-      alert("Uau! Aprovado com distinção");
+      alert("Uau! Aluno aprovado com distinção!");
      else
-      alert("Parabens, aprovado! Media "+media);
+      alert("Aluno aprovado! Media "+media);
     else
-     alert("Reprovado!")
+     alert("Aluno reprovado!")
 
    }
